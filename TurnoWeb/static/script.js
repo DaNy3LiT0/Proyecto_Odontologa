54bd52bc0e32315149ce8f6129478baeb120a1e1
DaNy3LiT0 <daniel-5586@hotmail.com.ar>
2024-06-24 09:28:50 -0300

    Modificacion Tarjetas
    funcionan ok

--- TurnoWeb/static/script.js
+++ TurnoWeb/static/script.js
@@ -1,19 +1,13 @@
-// Función para cambiar el estado de las tarjetas
 function toggleCard() {
-  var flipCard = document.querySelector('.flip-card');
-  var front = flipCard.querySelector('.front');
-  var back = flipCard.querySelector('.back');
+    var flipCard = document.querySelector('.flip-card');
+    var front = flipCard.querySelector('.front');
+    var back = flipCard.querySelector('.back');
 
-  // Cambia la visibilidad de las tarjetas
-  if (front.style.visibility === 'visible') {
-    front.style.display = 'none';
-    front.style.visibility = 'hidden';
-    back.style.display = 'block';
-    back.style.visibility = 'visible';
-  } else {
-    front.style.display = 'block';
-    front.style.visibility = 'visible';
-    back.style.display = 'none';
-    back.style.visibility = 'hidden';
-  }
+    if (front.style.display === 'block' || front.style.display === '') {
+        front.style.display = 'none';
+        back.style.display = 'block';
+    } else {
+        front.style.display = 'block';
+        back.style.display = 'none';
+    }
 }
